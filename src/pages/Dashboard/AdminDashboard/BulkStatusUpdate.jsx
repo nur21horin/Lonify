b1825c4183a477ccd56e54cb17cfd12511ccef2f
Nur Mohammod <nurm98371@gmail.com>
2025-12-18 22:49:00 +0600

    add

--- src/pages/Dashboard/AdminDashboard/BulkStatusUpdate.jsx
+++ src/pages/Dashboard/AdminDashboard/BulkStatusUpdate.jsx
@@ -13,7 +13,7 @@ const BulkStatusUpdate = ({ selectedApps, onBulkUpdate }) => {
       showCancelButton: true,
       confirmButtonText: "Confirm",
     });
-// NUR
+// NUR Mohammod
     if (res.isConfirmed) {
       onBulkUpdate(status);
     }
